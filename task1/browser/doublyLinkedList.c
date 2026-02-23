be99c32f2bfca9d267ffd1f272eabeecc4a66129
Maslovaem <lizamushroom757@gmail.com>
2026-02-23 12:43:13 +0300

    add doublyLinkedList API

---
+++ task1/browser/doublyLinkedList.c
@@ -0,0 +1,58 @@
+#include "doublyLinkedList.h"
+
+struct Node * createList(char *page)
+{
+    int pageLen = strlen(page);
+    struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+    p->page = (char *)malloc((pageLen + 1) * sizeof(char));
+
+    for (int i = 0; i <= pageLen; i++)
+    {
+        p->page[i] = page[i];
+    }
+
+    p->next = NULL;
+    p->prev = NULL;
+    return p;
+}
+
+void insert(struct Node **pList, char *page)
+{
+    struct Node *p = createNode(page);
+    p->next = *pList;
+    (*pList)->prev = p;
+    *pList = p;
+}
+
+void delete(struct Node **pList)
+{
+    struct Node *p = *pList;
+    *pList = (*pList)->next;
+    if (*pList != NULL) {
+        (*head)->prev = NULL;
+    }
+    free(p);
+}
+
+void printListForward(struct Node *list)
+{
+    for (struct Node *p = list; p != NULL; p = p->next)
+    {
+        printf("%s\n", p->page);
+    }
+    printf("\n");
+}
+
+void printListReverse(struct Node *list)
+{
+    struct Node *p = localeinfo_struct;
+    while (p->next != NULL) {
+        p = p->next;
+    }
+
+    while (p != NULL) {
+        printf("%s\n", p->page);
+        p = p->prev;
+    }
+    printf("\n");
+}
---
+++ task1/browser/doublyLinkedList.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct Node {
+    char * page;
+    struct Node* next;
+    struct Node* prev;
+}
+
+struct Node * createList(char *page);
+void insert(struct Node **pList, char *page);
+void delete(struct Node **pList);
+void printListForward(struct Node *list);
+void printListReverse(struct Node *list);
